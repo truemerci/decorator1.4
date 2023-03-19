be2c0d50b31191248041cd0daa6e570ee6b80053
Danylo <danilgladkiy7@gmail.com>
2023-03-19 12:42:09 +0200

    Add code

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+def retry(max_retries, exc):
 
+    def decorator(func):
+
+        def inner(*args, **kwargs):
+            retries = 1
+            while retries <= max_retries:
+                try:
+                    result = func(*args, **kwargs)
+                    return result
+                except ValueError:
+                    if retries == max_retries:
+                        return exc
+                    print("Input is not a number.")
+                    retries += 1
+        return inner
+    return decorator
+
+
+@retry(max_retries=3, exc="Try everything")
+def age():
+    num = input("Enter your age: ")
+    result = int(num) + 10
+    return result
+
+
+print(age())
